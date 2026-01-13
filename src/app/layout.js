ff6145f55fa7dfdf815bad31b656a535e17388c1
Maximilian-D-Muhr <maximilian.muhr@me.com>
2026-01-13 13:59:48 +0100

    feat: add navigation bar with Home and Cart links
    - Create Navigation component using DaisyUI navbar
    - Add Navigation to root layout (available on all pages)
    - Style layout with container and proper spacing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navigation from "@/components/Navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navigation />
+        <main className="container mx-auto px-4 py-8 min-h-screen">
+          {children}
+        </main>
       </body>
     </html>
   );
